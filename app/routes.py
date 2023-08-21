ed12709b5d3e6f285714f98aa8d94fe717f50b9b
Mitchell Hamm <www.zanehamm@yahoo.com>
2023-08-21 15:10:52 -0700

    added database and auth

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,70 @@
-from flask import request, render_template
+from flask import request, render_template, flash, redirect, url_for
 import requests
 from app import app
 from .forms import SearchForm, LoginForm, SignupForm
+from flask_login import login_user, logout_user, current_user, login_required
+from .models import User, db
+from werkzeug.security import check_password_hash
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+# fake database
+REGISTERED_USERS = {
+   'test@thieves.com': {
+      'name': 'test',
+      'password': 'test'
+   }
+}
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data.lower()
+        password = form.password.data
+        queried_user = User.query.filter(User.email == email).first()
+        if queried_user and check_password_hash(queried_user.password, password):
+           login_user(queried_user)
+           flash(f'Hello, {queried_user.first_name}', 'info')
+           return redirect(url_for('home'))
+        else:
+           flash('Invalid email or password', 'danger')
+           return redirect(url_for('login'))
     return render_template('login.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
-    return render_template('signup.html', form=form)
+    if request.method == 'POST' and form.validate_on_submit():
+
+        # Grabbing our sign up form data
+        first_name = form.first_name.data.title() 
+        last_name = form.last_name.data.title()
+        email = form.email.data.lower()
+        password = form.password.data
+        
+        # Creating an instance of the User Model
+        new_user = User(first_name, last_name, email, password)
+
+        # Adding new user to our database
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash(f'Thank you for signing up {new_user.first_name}!', 'info')
+        return redirect(url_for('login'))
+    else:
+        return render_template('signup.html', form=form)
+    
+@app.route('/logout')
+def logout():
+   logout_user()
+   return redirect(url_for('home'))
 
 @app.route('/pokemon/info', methods=['GET', 'POST'])
+@login_required
 def get_pokemon_info():
     form = SearchForm()
     if request.method == 'POST' and form.validate_on_submit():
